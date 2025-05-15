c79fdee24bc5a2c521219104734f8889f751d193
Sarthak Pradhan <126698343+sarthakp107@users.noreply.github.com>
2025-05-15 12:31:54 +1000

    Time: 16 ms (22.57%), Space: 49.6 MB (6.47%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,40 +1,61 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+        //count the frequency and put it in the hashmap
+        /**
+        hashmap:
+        Number | Frequency
+        1           3
+        2           2
+        3           1
 
-        for(int i = 1 ; i <= nums.length ; i++){
+        now i have to sort this in descending order and find the first k element
+        this will take logn time complexity
+
+        to do this in O(n) i can use bucket sort
+
+           hashmap:
+        Number | Frequency
+        1           3
+        2           2
+        3           1
+
+        then store it in the bucket array in that index 
+        bucket[1] = 3
+        bucket[2] = 2
+        bucket[3] = 1
+         */
+
+         HashMap<Integer, Integer> map = new HashMap<>();
+
+         for(int num: nums){
+            map.put(num , map.getOrDefault(num, 0) + 1);
+         }
+
+         //a bucket can store multiple elements
+         //bucket[i] = 3,2
+         List<Integer>[] bucket = new ArrayList[nums.length + 1];
+         for(int i = 0; i <= nums.length; i++){
             bucket[i] = new ArrayList<>();
-        }
+         }
 
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        /**
-        [5,5,6,6,6,7,8]
-         */
+         for(Map.Entry<Integer, Integer> m : map.entrySet()){
+            bucket[m.getValue()].add(m.getKey());
+         }
 
-        /**
-        5 - 2
-        6 - 3
-        7 - 1
-        8 - 1
-         */
+         //loop from end -> as wee need most frequent element
+         //till index < k
 
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            bucket[entry.getValue()].add(entry.getKey());
-        }
-        int[] res = new int[k];
-        int index = 0;
-        //loop start from the last, 
-        for(int i = bucket.length - 1; i > 0 && index <= k ; i--){
+         int[] res = new int[k];
+         int index = 0;
+         for(int i = bucket.length - 1; i >= 0 && index < k ; i--){
             for(int n : bucket[i]){
                 res[index++] = n;
                 if(index == k){
                     return res;
                 }
             }
-        }
-        return res;
+         }
+         return res;
+
     }
 }
